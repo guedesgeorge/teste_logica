0185cfdb4dc9032d9ed4ade7c628a3bc1f1cf8cf
´´George <george.emannuelcarvalho@gmail.com>
2024-01-20 00:23:10 -0300

    logica

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas Vindas ao jogo do número secreto');
+let numeroSecreto = 29;
+let  chute = prompt('Escolha um numero de 1 e 30');
+
+if (chute == numeroSecreto); {
+    console.log('Isso ai! você descobriu o numero secreto (29) ');
+}
